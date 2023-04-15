a6266988badb3c763723040576d3f161e172f540
Danielborbafs <dev.danielborba@gmail.com>
2023-04-15 05:48:32 -0300

    rotas

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const mysql2 = require('mysql2')
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const db = mysql2.createConnection({
     host: 'localhost',
@@ -10,7 +12,6 @@ const db = mysql2.createConnection({
     password: '',
     database: 'painelcrud'
 });
-
 
 // Testa a conexão com o banco de dados
 db.connect((err) => {
@@ -22,7 +23,10 @@ db.connect((err) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Cria a rota de cadastro
+// Chave secreta para assinatura do token
+const secretKey = 'minhaChaveSecreta123';
+
+// Rota de cadastro
 app.post('/cadastro', (req, res) => {
     // Recebe os dados do formulário de cadastro
     const { nome, email, senha } = req.body;
@@ -33,17 +37,75 @@ app.post('/cadastro', (req, res) => {
         return;
     }
 
-    // Insere os dados no banco de dados
-    db.query('INSERT INTO cadastro_usuario (nome, email, senha) VALUES (?, ?, ?)', [nome, email, senha], (err, result) => {
+    // Cria o hash da senha
+    bcrypt.hash(senha, 10, (err, hash) => {
         if (err) {
-            console.log(err); // imprime o erro no console
-            res.status(500).send('Erro ao inserir os dados no banco de dados.');
+            console.log(err);
+            res.status(500).send('Erro ao criar hash da senha.');
             return;
         }
-    
-        res.status(200).send('Cadastro realizado com sucesso!');
+
+        // Insere os dados no banco de dados
+        db.query('INSERT INTO cadastro_usuario (nome, email, senha) VALUES (?, ?, ?)', [nome, email, hash], (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Erro ao inserir os dados no banco de dados.');
+                return;
+            }
+
+            res.status(200).send('Cadastro realizado com sucesso!');
+        });
     });
 });
+
+
+// Rota de login
+app.post('/login', (req, res) => {
+  // Recebe os dados do formulário de login
+  const { email, senha } = req.body;
+
+  // Verifica se todos os campos foram preenchidos
+  if (!email || !senha) {
+    res.status(400).send('Por favor, preencha todos os campos.');
+    return;
+  }
+
+  // Busca o usuário no banco de dados pelo email
+  db.query('SELECT * FROM cadastro_usuario WHERE email = ?', [email], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Erro ao buscar usuário.');
+      return;
+    }
+
+    if (result.length === 0) {
+      res.status(401).send('Usuário não encontrado.');
+      return;
+    }
+
+    const usuario = result[0];
+
+    // Verifica se a senha está correta
+    bcrypt.compare(senha, usuario.senha, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Erro ao comparar senhas.');
+        return;
+      }
+
+      if (!result) {
+        res.status(401).send('Senha incorreta.');
+        return;
+      }
+
+      // Gera um token para o usuário
+      const token = jwt.sign({ id: usuario.id }, secretKey, { expiresIn: '1h' });
+
+      res.status(200).json({ token });
+    });
+  });
+});
+
 
 
 app.listen(port, () => {
